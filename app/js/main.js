68b5985625fb1c6fffb641d9b5b18ec8d2762e2e
ThunderInTheGrass <propeller8924@gmail.com>
2022-10-15 14:58:09 +0800

    ---

--- app/js/main.js
+++ app/js/main.js
@@ -373,3 +373,35 @@ $(document).ready(function(){
     arrows: false,
   });
 });
+
+
+function modalOrder() {
+  const btns = document.querySelectorAll(".btn-order");
+  const modalOverlay = document.querySelector(".modal-overlay-order");
+  const modals = document.querySelectorAll(".modal-order");
+
+  btns.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      let path = e.currentTarget.getAttribute("data-path");
+
+      modals.forEach((el) => {
+        el.classList.remove("modal--visible");
+      });
+
+      document.querySelector(`[data-target="${path}"]`).classList.add("modal--visible");
+      modalOverlay.classList.add("modal-overlay--visible");
+    });
+  });
+
+  modalOverlay.addEventListener("click", (e) => {
+    console.log(e.target);
+
+    if (e.target == modalOverlay) {
+      modalOverlay.classList.remove("modal-overlay--visible");
+      modals.forEach((el) => {
+        el.classList.remove("modal--visible");
+      });
+    }
+  });
+}
+modalOrder();
