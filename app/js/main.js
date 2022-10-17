81d29bee39affcd9d46836bbf710038ecbf65340
ThunderInTheGrass <propeller8924@gmail.com>
2022-10-17 14:52:34 +0800

    ---

--- app/js/main.js
+++ app/js/main.js
@@ -190,54 +190,6 @@ function tabBodyInfo() {
 }
 tabBodyInfo();
 
-// function slider() {
-//   const casesTabBody = document.querySelectorAll(".cases__tab-body");
-//   let count = 0;
-
-//   for (let i = 0; i < casesTabBody.length; i++) {
-//     //обертка кнопок фильтров
-//     let nameTab = casesTabBody[i].querySelectorAll(".name-tab");
-//     let bodyInfo = casesTabBody[i].querySelectorAll(".body-info");
-//     let btnPrev = casesTabBody[i].querySelector(".btn-prev");
-//     let btnNext = casesTabBody[i].querySelector(".btn-next");
-
-//     console.log(btnPrev);
-
-//     btnNext.addEventListener("click", () => {
-//       if (count < nameTab.length - 1) {
-//         count += 1;
-//       } else {
-//         count = 0;
-//       }
-//       for (let k = 0; k < nameTab.length; k++) {
-//         // nameTab[k].classList.add('off');
-//         // nameTab[count].classList.remove('off');
-//         nameTab[k].classList.remove("tab--active");
-//         nameTab[count].classList.add("tab--active");
-//         bodyInfo[k].classList.remove("d-flex");
-//         bodyInfo[count].classList.add("d-flex");
-//         console.log(count);
-//       }
-//     });
-
-//     btnPrev.addEventListener("click", () => {
-//       if (count > 0) {
-//         count -= 1;
-//       } else {
-//         count = nameTab.length - 1;
-//       }
-//       for (let k = 0; k < nameTab.length; k++) {
-//         nameTab[k].classList.add("off");
-//         nameTab[count].classList.remove("off");
-//         bodyInfo[k].classList.remove("d-flex");
-//         bodyInfo[count].classList.add("d-flex");
-//         console.log(count);
-//       }
-//     });
-//   }
-// }
-// slider();
-
 function scrollToLink() {
   const anchors = document.querySelectorAll('a[href*="#"]');
 
@@ -424,3 +376,51 @@ function modalOrder() {
   });
 }
 modalOrder();
+
+function slider() {
+  const casesTabBody = document.querySelectorAll(".cases__tab-body");
+  let count = 0;
+
+  for (let i = 0; i < casesTabBody.length; i++) {
+    //обертка кнопок фильтров
+    let nameTab = casesTabBody[i].querySelectorAll(".name-tab");
+    let bodyInfo = casesTabBody[i].querySelectorAll(".body-info");
+    let btnPrev = casesTabBody[i].querySelector(".btn-prev");
+    let btnNext = casesTabBody[i].querySelector(".btn-next");
+
+    console.log(btnPrev);
+
+    btnNext.addEventListener("click", () => {
+      if (count < nameTab.length - 1) {
+        count += 1;
+      } else {
+        count = 0;
+      }
+      for (let k = 0; k < nameTab.length; k++) {
+        // nameTab[k].classList.add('off');
+        // nameTab[count].classList.remove('off');
+        nameTab[k].classList.remove("tab--active");
+        nameTab[count].classList.add("tab--active");
+        bodyInfo[k].classList.remove("d-flex");
+        bodyInfo[count].classList.add("d-flex");
+        console.log(count);
+      }
+    });
+
+    btnPrev.addEventListener("click", () => {
+      if (count > 0) {
+        count -= 1;
+      } else {
+        count = nameTab.length - 1;
+      }
+      for (let k = 0; k < nameTab.length; k++) {
+        nameTab[k].classList.add("off");
+        nameTab[count].classList.remove("off");
+        bodyInfo[k].classList.remove("d-flex");
+        bodyInfo[count].classList.add("d-flex");
+        console.log(count);
+      }
+    });
+  }
+}
+slider();
